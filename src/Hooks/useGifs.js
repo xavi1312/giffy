13d5d04c71fbf91fbf52c11f70bfa47575e87901
Xavi <xavierisachalemany@gmail.com>
2020-07-16 12:09:32 +0200

    implements last seach when user back to home

--- src/Hooks/useGifs.js
+++ src/Hooks/useGifs.js
@@ -4,20 +4,28 @@ import GifContext from 'Context/GifsContext';
 
 const INITIAL_PAGE = 0;
 
-const useGifs = ({ keyword = 'pokemon' }) => {
+const useGifs = ({ keyword }) => {
   const { gifs, setGifs } = useContext(GifContext);
   const [page, setPage] = useState(0);
 
+  // Get keyword from localStorage
+  const keywordToUse =
+    keyword || localStorage.getItem('lastKeyword') || 'patata';
+
   useEffect(() => {
-    getGifs({ keyword }).then(setGifs);
-  }, [keyword, setGifs]);
+    getGifs({ keyword: keywordToUse }).then(gifs => {
+      // Save keyword to localStorage
+      localStorage.setItem('lastKeyword', keywordToUse);
+      setGifs(gifs);
+    });
+  }, [keyword, keywordToUse, setGifs]);
 
   useEffect(() => {
     if (page === INITIAL_PAGE) return;
-    getGifs({ keyword, page }).then(nextGifs => {
+    getGifs({ keyword: keywordToUse, page }).then(nextGifs => {
       setGifs(prevGifs => prevGifs.concat(nextGifs));
     });
-  }, [keyword, page, setGifs]);
+  }, [keyword, keywordToUse, page, setGifs]);
 
   return { gifs, setPage };
 };
